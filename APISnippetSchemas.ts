2d42060b12461c1db67d15b270130f1f862b1b31
Win Cheng <winzamark12@gmail.com>
2025-09-05 10:35:44 -0700

    update paths to support v1 and v2

--- APISnippetSchemas.ts
+++ APISnippetSchemas.ts
@@ -12,7 +12,7 @@ export const APISchemas: {
   [key: string]: APIType;
 } = {
   "ai-scrape": {
-    path: "/ai/scrape",
+    path: "/v1/ai/scrape",
     method: "POST",
     body: {
       url: "https://news.ycombinator.com/news",
@@ -21,7 +21,7 @@ export const APISchemas: {
     sdk_key_string: "web.ai_scrape",
   },
   sentiment: {
-    path: "/ai/sentiment",
+    path: "/v1/ai/sentiment",
     method: "POST",
     body: {
       text: "I love this product! It's amazing but the delivery was a bit late.",
@@ -29,7 +29,7 @@ export const APISchemas: {
     sdk_key_string: "sentiment",
   },
   classification: {
-    path: "/classification",
+    path: "/v1/classification",
     method: "POST",
     body: {
       dataset: [
@@ -44,7 +44,7 @@ export const APISchemas: {
     sdk_key_string: "classification",
   },
   translate: {
-    path: "/ai/translate",
+    path: "/v1/ai/translate",
     method: "POST",
     body: {
       text: ["Hello", "How are you?", "Thank you"],
@@ -53,7 +53,7 @@ export const APISchemas: {
     sdk_key_string: "translate.text",
   },
   summary: {
-    path: "/ai/summary",
+    path: "/v1/ai/summary",
     method: "POST",
     body: {
       text: "The Leaning Tower of Pisa, or simply, the Tower of Pisa, is the campanile, or freestanding bell tower, of Pisa Cathedral. It is situated behind the Cathedral and is the third-oldest structure in the city's Cathedral Square, after the Cathedral and the Baptistry. The tower's tilt began during construction in the 12th century, caused by an inadequate foundation on ground too soft on one side to properly support the structure's weight. The tilt increased in the decades before the structure was completed in the 14th century. It gradually increased until the structure was stabilized by efforts in the late 20th and early 21st centuries. The height of the tower is 55.86 metres (183.27 feet) from the ground on the low side and 56.67 metres (185.93 feet) on the high side. The width of the walls at the base is 2.44 m (8 ft 0.06 in). Its weight is estimated at 14,500 tonnes. The tower has 296 or 294 steps; the seventh floor has two fewer steps on the north-facing staircase.",
@@ -63,7 +63,7 @@ export const APISchemas: {
     sdk_key_string: "summary",
   },
   prediction: {
-    path: "/ai/prediction",
+    path: "/v1/ai/prediction",
     method: "POST",
     body: {
       dataset: [
@@ -78,7 +78,7 @@ export const APISchemas: {
     sdk_key_string: "prediction",
   },
   sql: {
-    path: "/ai/sql",
+    path: "/v1/ai/sql",
     method: "POST",
     body: {
       database: "postgresql",
@@ -97,7 +97,7 @@ export const APISchemas: {
     sdk_key_string: "text_to_sql",
   },
   "ai-search": {
-    path: "/web/search",
+    path: "/v1/web/search",
     method: "POST",
     body: {
       query: "What is the capital of France?",
@@ -105,7 +105,7 @@ export const APISchemas: {
     sdk_key_string: "web.search",
   },
   "search-suggestions": {
-    path: "/web/search/suggest",
+    path: "/v1/web/search/suggest",
     method: "GET",
     query: {
       query: "What is the capital",
@@ -113,7 +113,7 @@ export const APISchemas: {
     sdk_key_string: "web.search_suggestions",
   },
   embedding: {
-    path: "/embedding",
+    path: "/v1/embedding",
     method: "POST",
     body: {
       text: "Caption: Secluded Tiki Bar on a serene lake, perfect for a refreshing drink and a relaxing getaway",
@@ -121,8 +121,17 @@ export const APISchemas: {
     },
     sdk_key_string: "embedding",
   },
+  embeddingV2: {
+    path: "/v2/embedding",
+    method: "POST",
+    body: {
+      text: "Caption: Secluded Tiki Bar on a serene lake, perfect for a refreshing drink and a relaxing getaway",
+      type: "text",
+    },
+    sdk_key_string: "embeddingV2",
+  },
   "object-detection": {
-    path: "/object_detection",
+    path: "/v1/object_detection",
     method: "POST",
     body: {
       url: "https://jigsawstack.com/preview/object-detection-example-input.jpg",
@@ -134,7 +143,7 @@ export const APISchemas: {
     sdk_key_string: "vision.object_detection",
   },
   vocr: {
-    path: "/vocr",
+    path: "/v1/vocr",
     method: "POST",
     body: {
       prompt: ["total_price", "tax"],
@@ -143,7 +152,7 @@ export const APISchemas: {
     sdk_key_string: "vision.vocr",
   },
   "speech-to-text": {
-    path: "/ai/transcribe",
+    path: "/v1/ai/transcribe",
     method: "POST",
     body: {
       url: "https://jigsawstack.com/preview/stt-example.wav",
@@ -151,7 +160,7 @@ export const APISchemas: {
     sdk_key_string: "audio.speech_to_text",
   },
   "spam-check": {
-    path: "/validate/spam_check",
+    path: "/v1/validate/spam_check",
     method: "POST",
     body: {
       text: "Congratulations! You've won a free iPhone! Click here to claim now!",
@@ -159,7 +168,7 @@ export const APISchemas: {
     sdk_key_string: "validate.spam_check",
   },
   nsfw: {
-    path: "/validate/nsfw",
+    path: "/v1/validate/nsfw",
     method: "POST",
     body: {
       url: "https://images.unsplash.com/photo-1633998979521-11ca9d0e6e38?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -167,7 +176,7 @@ export const APISchemas: {
     sdk_key_string: "validate.nsfw",
   },
   profanity: {
-    path: "/validate/profanity",
+    path: "/v1/validate/profanity",
     method: "POST",
     body: {
       text: "This is a sample text that might contain inappropriate language.",
@@ -175,7 +184,7 @@ export const APISchemas: {
     sdk_key_string: "validate.profanity",
   },
   spellcheck: {
-    path: "/validate/spell_check",
+    path: "/v1/validate/spell_check",
     method: "POST",
     body: {
       text: "This sentense has a speling mistake.",
@@ -183,7 +192,7 @@ export const APISchemas: {
     sdk_key_string: "validate.spellcheck",
   },
   "file-upload": {
-    path: "/store/file",
+    path: "/v1/store/file",
     method: "POST",
     body: {
       blob: "image-123.png",
@@ -193,7 +202,7 @@ export const APISchemas: {
     skip_request: true,
   },
   "file-get": {
-    path: "/store/file/read/image-123.png",
+    path: "/v1/store/file/read/image-123.png",
     method: "GET",
     query: {
       key: "image-123.png",
@@ -202,7 +211,7 @@ export const APISchemas: {
     skip_request: true,
   },
   "file-delete": {
-    path: "/store/file/read/image-123.png",
+    path: "/v1/store/file/read/image-123.png",
     method: "DELETE",
     query: {
       key: "image-123.png",
@@ -211,7 +220,7 @@ export const APISchemas: {
     skip_request: true,
   },
   "image-generation": {
-    path: "/ai/image_generation",
+    path: "/v1/ai/image_generation",
     method: "POST",
     body: {
       prompt: "A beautiful sunset over a calm ocean",
@@ -220,7 +229,7 @@ export const APISchemas: {
     sdk_key_string: "image_generation",
   },
   "image-translate": {
-    path: "/ai/translate/image",
+    path: "/v1/ai/translate/image",
     method: "POST",
     body: {
       url: "https://images.unsplash.com/photo-1528716321680-815a8cdb8cbe?q=80&w=1376&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
@@ -230,7 +239,7 @@ export const APISchemas: {
     sdk_key_string: "translate.image",
   },
   "html-to-any": {
-    path: "/web/html_to_any",
+    path: "/v1/web/html_to_any",
     method: "POST",
     body: {
       url: "https://news.ycombinator.com/",
@@ -239,7 +248,7 @@ export const APISchemas: {
     sdk_key_string: "web.html_to_any",
   },
   "deep-research": {
-    path: "/web/deep_research",
+    path: "/v1/web/deep_research",
     method: "POST",
     body: {
       query: "What is the meaning of life?",
